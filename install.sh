fd983887708b3cdf7e4e8bf06cb441d700638576
glaulher <glaulher.developer@gmail.com>
2021-08-03 22:44:14 -0300

    update

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ xbps-reconfigure -f linux
 ln -s /etc/sv/dbus /var/service
 ln -s /etc/sv/elogind /var/service
 
-xbps-install -S nano vim Adapta papirus-icon-theme breeze-cursors qt5-styleplugins font-firacode fonts-roboto-ttf font-awesome
-xbps-install -S pulseaudio pavucontrol alsa-plugins-pulseaudio alsa-utils squashfs-tools xrdb xkill gvfs acpilight
-xbps-install -S firefox-i18n-pt-BR ranger trash-cli mpv feh lrzip unzip zip p7zip-unrar p7zip  
+xbps-install -Sy nano vim Adapta papirus-icon-theme breeze-cursors qt5-styleplugins font-firacode fonts-roboto-ttf font-awesome
+xbps-install -Sy pulseaudio pavucontrol alsa-plugins-pulseaudio alsa-utils squashfs-tools xrdb xkill gvfs acpilight
+xbps-install -Sy firefox-i18n-pt-BR ranger trash-cli mpv feh lrzip unzip zip p7zip-unrar p7zip  
